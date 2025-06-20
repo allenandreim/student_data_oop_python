e38c0ad5e266955b84f18f49815904889cacede0
allenandreim <allenandrei8209@gmail.com>
2025-06-21 00:03:21 +0800

    parent_code

--- student_data.py
+++ student_data.py
@@ -1,16 +1,30 @@
-# Parent class
-
 class Student:
-    def __init__(self, name, ID, age, mood, grade):
+    def __init__(self, name, student_id, age, mood, grade, salary=0):
         self.name = name
-        self.ID = ID
-        self.age = age
+        self.student_id = student_id
+        self.age = int(age)
         self.mood = mood
-        self.grade = grade
+        self.grade = float(grade)
+        self.__salary = salary  # encapsulated
 
     def say_hi(self):
-        print("Name:", self.name,
-                "\nID:", self.ID,
-                "\nAge:", self.age,
-                "\nMood:", self.mood,
-                "\nGrade:", self.grade)
+        print("Name:", self.name)
+        print("ID:", self.student_id)
+        print("Age:", self.age)
+        print("Mood:", self.mood)
+        print("Grade:", self.grade)
+
+    def grow(self):
+        self.age += 1
+
+    def get_age(self):
+        print(f"Current age is: {self.age}")
+
+    def get_salary_expectation(self):
+        print(f"My salary expectation is: {self.__salary}")
+
+    def __repr__(self):
+        return f"{self.name} is {self.age} years old and achieved {self.grade} in the exam."
+
+    def to_file_format(self):
+        return f"{self.name},{self.student_id},{self.age},{self.mood},{self.grade},{self.__salary}\n"
